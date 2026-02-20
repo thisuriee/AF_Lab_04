c06b8bfa9cabad25230e6046130c4d68064d0213
Mona Lisa <168088147+thisuriee@users.noreply.github.com>
2026-02-20 21:25:02 +0530

    Promises Added

--- app.js
+++ app.js
@@ -43,5 +43,25 @@
 //   console.log("Error: " + err.message);
 // });
 
-const myModule = require('./my-module.js');
-console.log(myModule.myFunction());
+//Modules
+// const myModule = require('./my-module.js');
+// console.log(myModule.myFunction());
+
+//Promises
+const condition = true;
+
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve('Success!');
+  } else {
+    reject('Failure!');
+  }
+});
+
+myPromise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
